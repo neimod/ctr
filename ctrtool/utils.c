ed881594521bcc143f55552b02acedce1d0abf18
neimod <neimod@gmail.com>
2011-07-12 13:22:50 +0200

    moved memdump and readkeyfile to utils

--- ctrtool/utils.c
+++ ctrtool/utils.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include "utils.h"
 
 
@@ -30,4 +32,66 @@ u32 getle32(const u8* p)
 u32 getle16(const u8* p)
 {
 	return (p[0]<<0) | (p[1]<<8);
+}
+
+
+void readkeyfile(u8* key, const char* keyfname)
+{
+	FILE* f = fopen(keyfname, "rb");
+	u32 keysize = 0;
+
+	if (0 == f)
+	{
+		fprintf(stdout, "Error opening key file\n");
+		goto clean;
+	}
+
+	fseek(f, 0, SEEK_END);
+	keysize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	if (keysize != 16)
+	{
+		fprintf(stdout, "Error key size mismatch, got %d, expected %d\n", keysize, 16);
+		goto clean;
+	}
+
+	if (16 != fread(key, 1, 16, f))
+	{
+		fprintf(stdout, "Error reading key file\n");
+		goto clean;
+	}
+
+clean:
+	if (f)
+		fclose(f);
+}
+
+
+void memdump(FILE* fout, const char* prefix, const u8* data, u32 size)
+{
+	u32 i;
+	u32 prefixlen = strlen(prefix);
+	u32 offs = 0;
+	u32 line = 0;
+	while(size)
+	{
+		u32 max = 16;
+
+		if (max > size)
+			max = size;
+
+		if (line==0)
+			fprintf(fout, "%s", prefix);
+		else
+			fprintf(fout, "%*s", prefixlen, "");
+
+
+		for(i=0; i<max; i++)
+			fprintf(fout, "%02X", data[offs+i]);
+		fprintf(fout, "\n");
+		line++;
+		size -= max;
+		offs += max;
+	}
 }
--- ctrtool/utils.h
+++ ctrtool/utils.h
@@ -14,6 +14,10 @@ u64 getle64(const u8* p);
 u32 getle32(const u8* p);
 u32 getle16(const u8* p);
 
+
+void readkeyfile(u8* key, const char* keyfname);
+void memdump(FILE* fout, const char* prefix, const u8* data, u32 size);
+
 #ifdef __cplusplus
 }
 #endif
