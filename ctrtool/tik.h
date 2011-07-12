a1cb6f2cadb2804ab2f3011a4983af8fe2c96111
blasty <blasty@fail0verflow.com>
2011-07-12 21:21:05 +0200

    Updated tik struct

--- ctrtool/tik.h
+++ ctrtool/tik.h
@@ -4,6 +4,11 @@
 #include "types.h"
 
 typedef struct {
+	u32 enable_timelimit;
+	u32 timelimit_seconds;
+} timelimit_entry;
+
+typedef struct {
 	u32 sig_type;
 	u8  signature[0x100];
 	u8  padding1[0x3c];
@@ -15,9 +20,17 @@ typedef struct {
 	u8  ticket_id[8];
 	u32 console_id;
 	u8  title_id[8];	
-	u8  unknown2[2];
+	u16 sys_access;
 	u16 ticket_version;
-		
+	u32 time_mask;
+	u32 permit_mask;
+	u8 title_export;
+	u8 commonkey_idx;
+	u8 unknown_buf[0x30];
+	u8 content_permissions[0x40];
+	u16 padding0;
+
+	timelimit_entry timelimits[8];	
 } eticket;
 
 void tik_print(const u8 *blob, u32 size);
