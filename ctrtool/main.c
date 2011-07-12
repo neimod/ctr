6cb95cbe0745ca12ed305b2dc8b7789ba4f98c31
neimod <neimod@gmail.com>
2011-07-12 13:10:25 +0200

    add u64

--- ctrtool/main.c
+++ ctrtool/main.c
@@ -83,17 +83,17 @@ static u32 align(u32 offset, u32 alignment)
 	return (offset + (alignment-1)) & mask;
 }
 
-unsigned long long getle64(const u8* p)
+u64 getle64(const u8* p)
 {
-	unsigned long long n = p[0];
+	u64 n = p[0];
 
-	n |= (unsigned long long)p[1]<<8;
-	n |= (unsigned long long)p[2]<<16;
-	n |= (unsigned long long)p[3]<<24;
-	n |= (unsigned long long)p[4]<<32;
-	n |= (unsigned long long)p[5]<<40;
-	n |= (unsigned long long)p[6]<<48;
-	n |= (unsigned long long)p[7]<<56;
+	n |= (u64)p[1]<<8;
+	n |= (u64)p[2]<<16;
+	n |= (u64)p[3]<<24;
+	n |= (u64)p[4]<<32;
+	n |= (u64)p[5]<<40;
+	n |= (u64)p[6]<<48;
+	n |= (u64)p[7]<<56;
 	return n;
 }
 
--- ctrtool/types.h
+++ ctrtool/types.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-typedef unsigned char  u8;
-typedef unsigned short u16;
-typedef unsigned int   u32;
-typedef unsigned int   u64;
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned int		u32;
+typedef unsigned long long	u64;
 
 #endif
